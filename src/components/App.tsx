138064cef1f2a4b676f0db674d3535e1580a4b64
Sid Doshi <sidoshi510@gmail.com>
2018-09-03 18:38:06 +0530

    Provide store at a higher level

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,8 @@
 import * as React from 'react'
-import { Provider } from 'react-redux'
-
-import { store } from '../store'
 
 class App extends React.Component {
   public render() {
-    return (
-      <Provider store={store}>
-        <h1>Hello world</h1>
-      </Provider>
-    )
+    return <h1>Hello world</h1>
   }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
+import { store } from './store'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+)
 registerServiceWorker()
