25b48c4ee95673d13f633982832f4b5067749352
Sid Doshi <sidoshi510@gmail.com>
2018-09-23 11:11:17 +0530

    Remove crap code

--- src/components/containers/BalanceSheet.tsx
+++ src/components/containers/BalanceSheet.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
-import styled from 'styled-components'
 
 import AddTransactionForm from '../AddTransactionForm'
-import EntriesList from '../EntriesList'
 import { Entries } from '../../types'
 
 import { selectDebitEntries, selectCreditEntries } from '../../selectors'
@@ -14,21 +12,9 @@ interface Props {
   debitEntries: Entries
 }
 
-const Con = styled.div`
-  display: flex;
-  justify-content: center;
-  div {
-    margin: 0px auto;
-  }
-`
-
 const BalaceSheet = ({ creditEntries, debitEntries }: Props) => (
   <div>
     <AddTransactionForm />
-    <Con>
-      <EntriesList entries={debitEntries} />
-      <EntriesList entries={creditEntries} />
-    </Con>
   </div>
 )
 
