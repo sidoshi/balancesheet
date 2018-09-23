891b444b929bfb9f580e6eb4ba6cf9ffda62b5f8
Sid Doshi <sidoshi510@gmail.com>
2018-09-23 22:23:18 +0530

    Use custom semantic build to fix scrollbar issue

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,13 +1,22 @@
 import { injectGlobal } from 'styled-components'
 import theme from './theme'
 
-import 'semantic-ui-css/semantic.min.css'
+// We are using a custom semantic-ui build here.
+// This is to fix the scroll bar issue. Semantic-UI generates styles for
+// scrollbar which is not desirable for some cases. Since css styles can not
+// be turned off, creating a custom build without those styles is the only
+// option.
+// TODO: Ideally, the build should be automated in some manner
+// See:
+// - https://github.com/Semantic-Org/Semantic-UI/issues/5738
+// - https://github.com/Metnew/suicrux/issues/27
+import '../../semantic/semantic.css'
 
 const ingectGlobalStyles = () => injectGlobal`
-body {
-  margin: 0;
-  color: ${theme.textPrimary};
-}
+  body {
+    margin: 0;
+    color: ${theme.textPrimary};
+  }
 `
 
 export default ingectGlobalStyles
