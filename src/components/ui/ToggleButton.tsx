7c1e103f4bf37d7a316c490be93892e64661c1cb
Sid Doshi <sidoshi510@gmail.com>
2018-09-26 23:54:06 +0530

    Dark theme for toggle button

--- src/components/ui/ToggleButton.tsx
+++ src/components/ui/ToggleButton.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { Button } from 'semantic-ui-react'
-import styled from 'styled-components'
+import styled, { withTheme } from 'styled-components'
 
 import StyledButton, { Props as ButtonProps } from './Button'
+import { Theme } from 'styles/theme'
 
 interface Props extends ButtonProps {
   toggleValues: [string, string]
@@ -12,6 +13,7 @@ interface Props extends ButtonProps {
     value: string,
     e: React.SyntheticEvent<HTMLButtonElement>
   ) => void
+  theme: Theme
 }
 
 const Or = styled(Button.Or)`
@@ -21,22 +23,35 @@ const Or = styled(Button.Or)`
   }
 `
 
-export default (props: Props) => (
-  <Button.Group>
-    <StyledButton
-      {...props}
-      primary={props.selectedIndex === 0}
-      onClick={props.onUpdate.bind(null, 0, props.toggleValues[0])}
-    >
-      {props.toggleValues[0]}
-    </StyledButton>
-    <Or text="" variant={props.variant} />
-    <StyledButton
-      {...props}
-      primary={props.selectedIndex === 1}
-      onClick={props.onUpdate.bind(null, 1, props.toggleValues[1])}
-    >
-      {props.toggleValues[1]}
-    </StyledButton>
-  </Button.Group>
-)
+const ToggleButton = (props: Props) => {
+  const { toggleValues, selectedIndex, onUpdate, theme, ...buttonProps } = props
+
+  const buttonStyles: React.CSSProperties = {}
+  if (props.variant === 'themed') {
+    buttonStyles.backgroundColor = theme.backgroundPrimary
+  }
+
+  return (
+    <Button.Group>
+      <StyledButton
+        {...buttonProps}
+        primary={props.selectedIndex === 0}
+        onClick={props.onUpdate.bind(null, 0, props.toggleValues[0])}
+        style={props.selectedIndex !== 0 ? buttonStyles : {}}
+      >
+        {props.toggleValues[0]}
+      </StyledButton>
+      <Or text="" variant={buttonProps.variant} />
+      <StyledButton
+        {...buttonProps}
+        primary={props.selectedIndex === 1}
+        onClick={props.onUpdate.bind(null, 1, props.toggleValues[1])}
+        style={props.selectedIndex !== 1 ? buttonStyles : {}}
+      >
+        {props.toggleValues[1]}
+      </StyledButton>
+    </Button.Group>
+  )
+}
+
+export default withTheme(ToggleButton)
