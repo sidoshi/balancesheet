06d18695bd6c0dcefe10a15ea1c2109271814c93
Sid Doshi <sidoshi510@gmail.com>
2018-09-29 13:49:02 +0530

    Create entries list component

---
+++ src/components/EntriesList.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import zip from 'lodash/zip'
+import { Table } from 'semantic-ui-react'
+
+import { Entries } from '../types'
+import inrFmt from '../utils/inrFmt'
+import capitalized from '../utils/capitalized'
+
+interface Props {
+  creditEntries: Entries
+  debitEntries: Entries
+  creditTotal: number
+  debitTotal: number
+}
+
+const Wrapper = styled.div`
+  max-width: 800px;
+  margin: 100px auto;
+`
+
+const Row = styled(Table.Row)`
+  &&& {
+    font-size: 16px;
+    .amount {
+      letter-spacing: 1px;
+    }
+  }
+`
+
+class EntriesList extends React.Component<Props> {
+  public render() {
+    const entries = zip(this.props.debitEntries, this.props.creditEntries)
+
+    return (
+      <Wrapper>
+        <Table inverted={true} celled={true} collapsing={false} padded="very">
+          <Table.Header>
+            <Row>
+              <Table.HeaderCell colSpan="2">Debit</Table.HeaderCell>
+              <Table.HeaderCell colSpan="2">Credit</Table.HeaderCell>
+            </Row>
+            <Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Amount</Table.HeaderCell>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Amount</Table.HeaderCell>
+            </Row>
+          </Table.Header>
+
+          <Table.Body>
+            {entries.map(([debitEntry, creditEntry]) => (
+              <Row>
+                <Table.Cell>
+                  {debitEntry ? capitalized(debitEntry.name) : ''}
+                </Table.Cell>
+                <Table.Cell className="amount">
+                  {debitEntry ? inrFmt(debitEntry.amount) : ''}
+                </Table.Cell>
+                <Table.Cell>
+                  {creditEntry ? capitalized(creditEntry.name) : ''}
+                </Table.Cell>
+                <Table.Cell className="amount">
+                  {creditEntry ? inrFmt(creditEntry.amount) : ''}
+                </Table.Cell>
+              </Row>
+            ))}
+          </Table.Body>
+          <Table.Footer>
+            <Row>
+              <Table.HeaderCell />
+              <Table.HeaderCell className="amount">
+                {inrFmt(this.props.debitTotal)}
+              </Table.HeaderCell>
+              <Table.HeaderCell />
+              <Table.HeaderCell className="amount">
+                {inrFmt(this.props.creditTotal)}
+              </Table.HeaderCell>
+            </Row>
+          </Table.Footer>
+        </Table>
+      </Wrapper>
+    )
+  }
+}
+
+export default EntriesList
--- src/components/containers/BalanceSheet.tsx
+++ src/components/containers/BalanceSheet.tsx
@@ -2,25 +2,46 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 
 import AddTransactionForm from '../AddTransactionForm'
+import EntriesList from '../EntriesList'
 import { Entries } from '../../types'
 
-import { selectDebitEntries, selectCreditEntries } from '../../selectors'
+import {
+  selectDebitEntries,
+  selectCreditEntries,
+  selectCreditEntriesTotal,
+  selectDebitEntriesTotal,
+} from '../../selectors'
 import { ApplicationState } from '../../store'
 
 interface Props {
   creditEntries: Entries
   debitEntries: Entries
+  creditTotal: number
+  debitTotal: number
 }
 
-const BalaceSheet = ({ creditEntries, debitEntries }: Props) => (
+const BalaceSheet = ({
+  creditEntries,
+  debitEntries,
+  creditTotal,
+  debitTotal,
+}: Props) => (
   <div>
     <AddTransactionForm />
+    <EntriesList
+      creditEntries={creditEntries}
+      debitEntries={debitEntries}
+      creditTotal={creditTotal}
+      debitTotal={debitTotal}
+    />
   </div>
 )
 
 const mapStateToProps = (state: ApplicationState) => ({
   creditEntries: selectCreditEntries(state),
   debitEntries: selectDebitEntries(state),
+  creditTotal: selectCreditEntriesTotal(state),
+  debitTotal: selectDebitEntriesTotal(state),
 })
 
 export default connect(mapStateToProps)(BalaceSheet)
